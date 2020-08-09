2b14ff1464958984cd5b0aad3d5c342d66699622
weicent <weicent_zhang@outlook.com>
2020-08-10 00:23:33 +0800

    更新MATLAB中EKF_Algorithm.m注释

--- Extented_Kalman_Filter/Matlab_code/EKF_Algorithm.m
+++ Extented_Kalman_Filter/Matlab_code/EKF_Algorithm.m
@@ -1,12 +1,11 @@
 function [xEKF, pEKF]=EKF_Algorithm(x, u, p, z, H, R, Q, JF, JH, dt)
-% 预测
 xPred=PredictNewStatus(x,u, dt);
-pPred=JF*p*JF.'+Q;% JF.'等效于transpose(JF)
-% 更新
+pPred=JF*p*JF.'+Q; % JF.' == transpose(JF)
+
 zPred=H*xPred;
 y=z-zPred;
 S=JH*pPred*JH.'+R;
-K=pPred*JH.'*S^(-1);% S^(-1)等效于inv(S)
+K=pPred*JH.'*S^(-1); % S^(-1) == inv(S)
 xEKF=xPred+K*y;
 pEKF=(eye(4)-K*JH)*pPred;
 end
